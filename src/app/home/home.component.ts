9bdae8f73eb21947571859264bb9819622b11a64
techmonk69 <techmonkdigitalsolutions@gmail.com>
2025-01-17 11:17:46 +0530

    Deploy to GitHub Pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,14 +13,14 @@ export class HomeComponent {
    // Images for the carousel
    carouselImages = [
   
-    { src: 'assets/q1.jpg', alt: 'Third Slide' },
-    { src: 'assets/q2.jpg', alt: 'Fourth Slide' },
-    { src: 'assets/p5.jpg', alt: 'Fifth Slide' },
-    { src: 'assets/p6.jpg', alt: 'Sixth Slide' },
-    { src: 'assets/p7.jpg', alt: 'Seventh Slide'},
+    { src: 'assets/p2.jpg', alt: 'Third Slide' },
+    { src: 'assets/p3.jpg', alt: 'Fourth Slide' },
+    { src: 'assets/p4.jpg', alt: 'Fifth Slide' },
+    { src: 'assets/p5.jpg', alt: 'Sixth Slide' },
+    { src: 'assets/p6.jpg', alt: 'Seventh Slide'},
     { src: 'assets/p7.jpg', alt: 'Eight Slide'},
     { src: 'assets/p1.jpg', alt: 'First Slide' },
-    { src: 'assets/p2.jpg', alt: 'Second Slide' }
+    { src: 'assets/p8.jpg', alt: 'Second Slide' }
   ];
 
   private intervalId: any;
